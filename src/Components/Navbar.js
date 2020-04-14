a29013aa9e3c3e59c9e27d0a331c58e1f2f29556
attainu-instructor/balaji-kamalesan-au3 <balajikamalesan@gmail.com>
2020-04-14 08:39:24 +0530

    Changed the colour of NAvbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,7 +2,7 @@ import React from 'react'
 
  function Navbar() {
     return (
-        <div className="navbar navbar-light bg-primary fixed-top">
+        <div className="navbar navbar-custom fixed-top">
         <div className="navbar-brand">
         <img src="https://nptel.ac.in/assets/nptel_assets/images/nptel-logo.png" height="30" className="d-inline-block align-top" alt="" />
          
